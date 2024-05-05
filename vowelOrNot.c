960b6b136e72b983955c1bcdd8bf9ae08a54c2ae
Zaheed <szasaa10@gmail.com>
2024-05-05 20:33:19 +0530

    changesVowelOrNot

--- vowelOrNot.c
+++ vowelOrNot.c
@@ -4,14 +4,14 @@ int main()
 {
     char a;
     scanf("%c",&a);
-    char vowel[] = {'a','e','i','o','u'};
+    char vowel[] = {'a','e','i','o','u','A','E','I','O','U'};
     
-    for(int i=0; i < 5;i++){
+    for(int i=0; i < 10;i++){
         if(a==vowel[i]){
             printf("%c is a vowel",a);
+            return 0;
         }
-        else{
-            printf("%c is not a vowel",a);
     }
+    printf("%c is not a vowel",a);
     return 0;
 }
