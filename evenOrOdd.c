01e1866e33c4cc2d2608965863bc280aa7415181
Zaheed <szasaa10@gmail.com>
2024-05-05 20:10:00 +0530

    evenOrOdd

---
+++ evenOrOdd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    if(a%2==0){
+        printf("%d is Even");
+    }
+    else{
+        printf("%d is Odd");
+    }
+    return 0;
+}
