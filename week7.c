e82d9cf71648fa53b23c760823897199fbb880ab
Zaheed <szasaa10@gmail.com>
2024-05-05 20:49:32 +0530

    week7

---
+++ week7.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a <= 7){
+    switch(a){
+        case 1:
+        printf("Sunday");
+        break;
+        case 2:
+        printf("Monday");
+        break;
+        case 3:
+        printf("Tuesday");
+        break;
+        case 4:
+        printf("Wednesday");
+        break;
+        case 5:
+        printf("Thursday");
+        break;
+        case 6:
+        printf("Friday");
+        break;
+        case 7:
+        printf("Saturday");
+        break;
+        }
+    }
+    else{
+        printf("The number must be less than 7");
+    }
+    return 0;
+}
